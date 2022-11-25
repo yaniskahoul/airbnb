d0873e092c9124fcdfe8dc69a9aaa7523fda3aa8
Yanis Kahoul <kahoul.yanis@gmail.com>
2022-11-25 14:47:28 +0100

    essai carte sur home page

--- divers/views.py
+++ divers/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 
 def home_view(request):
-    context = {
-        'test': "Ceci est un test"
-    }
-    return render(request,'divers/home_page.html', context=context)
+    return render(request,'divers/home_page.html')
 
 
